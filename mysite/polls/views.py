3a569674ff08e9da6885c97bf8479fa8eefb50b3
Jayavidhi Kumar <38183251+Jay-VD-K@users.noreply.github.com>
2023-02-11 15:03:20 -0500

    flder struc

--- mysite/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-        return HttpResponse("Hello, world. Polls index here.")
-
-# Create your views here.
